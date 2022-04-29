bb0b7b816555c4894baebdcc0fddcddbaad15684
Alexander Dawson <a.m.dawson87@hotmail.com>
2022-04-28 19:09:58 -0700

    techblogpush

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,5 +1,25 @@
 // Import libraries and models
 
+const router= require('express').Router()
+const req = require('express/lib/request')
+const{Comments}= require('../../models')
+const withAuth= require('../../utils/auth')
+
+
+
+// Write Get, Post, Put and Delete Routes 
+router.post('/', withAuth, (req, res,) => {
+Comments.create({
+    ...req.body,
+    userId: req.session.userId
+    }) .then(res => {
+        res.json()
+    }) .catch(error => {
+        res.status(500).json(error)
+    });
+})
+
 // Write Get(Optional), Post Route
 
 // Exports
+module.exports=router
--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,5 +1,48 @@
 // Import libraries and models
+const router= require('express').Router()
+const req = require('express/lib/request')
+const{Post}= require('../../models')
+const withAuth= require('../../utils/auth')
+
+
 
 // Write Get, Post, Put and Delete Routes 
-
+router.post('/', withAuth, (req, res,) => {
+Post.create({
+    ...req.body,
+    userId: req.session.userId
+    }) .then(res => {
+        res.json()
+    }) .catch(error => {
+        res.status(500).json(error)
+    });
+})
 // Exports
+
+router.put('/:id', withAuth, (req, res,) => {
+    Post.update(req.body,
+        { 
+            where: {
+                id: req.params.id
+            }
+        }) .then(res => {
+            res.json()
+        }) .catch(error => {
+            res.status(500).json(error)
+        });
+    })
+
+    router.delete('/:id', withAuth, (req, res,) => {
+        Post.destroy({
+            
+                where: {
+                    id: req.params.id
+                }
+            }) .then(res => {
+                res.json()
+            }) .catch(error => {
+                res.status(500).json(error)
+            });
+        })
+
+        module.exports=router
